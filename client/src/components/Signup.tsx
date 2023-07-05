140a4b8d6ab3d0c1ea4d8ef5977e12363b87e19a
Anubhav Bhambri <anubhavbhambri1@gmail.com>
2023-07-05 20:40:48 +0530

    implemented signup functionality

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { MouseEventHandler, useState } from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import Navbar from "./Navbar";
 import {
   Card,
@@ -11,12 +12,37 @@ import {
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
-import { Link } from "react-router-dom";
+import { Link, Router } from "react-router-dom";
 import { Checkbox } from "./ui/checkbox";
+import { auth } from "../firebase/firebase";
+import { useNavigate } from "react-router-dom";
 
 type LoginPageProps = {};
 
 const SignUpPage: React.FC<LoginPageProps> = () => {
+  const [inputs, setInputs] = useState({ email: "", password: "" });
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+  const navigate = useNavigate();
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSignup = async (e: MouseEventHandler<HTMLButtonElement>) => {
+    try {
+      const newUser = await createUserWithEmailAndPassword(
+        inputs.email,
+        inputs.password
+      );
+      if (!newUser) return;
+      navigate("/");
+    } catch (error: any) {
+      alert(error.message);
+    }
+    console.log(inputs);
+  };
+  console.log(inputs);
   return (
     <div>
       <Navbar />
@@ -29,9 +55,13 @@ const SignUpPage: React.FC<LoginPageProps> = () => {
         </CardHeader>
         <CardContent>
           <Label>Email</Label>
-          <Input className="mb-5" />
+          <Input className="mb-5" onChange={handleChangeInput} name="email" />
           <Label>Password</Label>
-          <Input className="mb-5" />
+          <Input
+            className="mb-5"
+            onChange={handleChangeInput}
+            name="password"
+          />
           <div className="flex items-center space-x-2 mb-3">
             <Checkbox id="terms" />
             <Label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
@@ -43,7 +73,7 @@ const SignUpPage: React.FC<LoginPageProps> = () => {
           <Link to="/login">
             <Button variant="outline">Login</Button>
           </Link>
-          <Button>Sign Up</Button>
+          <Button onClick={handleSignup}>Sign Up</Button>
         </CardFooter>
       </Card>
     </div>
