0ce80fb7e030d18db29b51a658cb01e09c05cfba
Anubhav Bhambri <anubhavbhambri1@gmail.com>
2023-07-07 15:09:30 +0530

    added functionality to logout button

--- client/src/components/Logout.tsx
+++ client/src/components/Logout.tsx
@@ -1,12 +1,19 @@
 import * as React from "react";
 import { Button } from "./ui/button";
 import { FiLogOut } from "react-icons/fi";
+import { useSignOut } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/firebase";
 
 interface ILogoutProps {}
 
 const Logout: React.FunctionComponent<ILogoutProps> = (props) => {
+  const [signOut, loading, error] = useSignOut(auth);
+
+  const handleLogout = () => {
+    signOut();
+  };
   return (
-    <Button>
+    <Button onClick={handleLogout}>
       <FiLogOut />
     </Button>
   );
